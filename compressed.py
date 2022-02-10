9db24baeeef606cba73f5c34be48aab8b8ae06fc
zhangzzu <zhangzzu@gmail.com>
2022-02-10 16:14:42 +0800

    add k-means function

--- compressed.py
+++ compressed.py
@@ -18,3 +18,18 @@ def compress_fun(T, device):
     out = torch.where(T <= -0.5, T, out_)
 
     return out
+
+def kmeans(x, ncluster, niter=10):
+    N, D = x.size()
+    c = x[torch.randperm(N)[:ncluster]] # init clusters at random
+    for i in range(niter):
+        # assign all pixels to the closest codebook element
+        a = ((x[:, None, :] - c[None, :, :])**2).sum(-1).argmin(1)
+        # move each codebook element to be the mean of the pixels that assigned to it
+        c = torch.stack([x[a==k].mean(0) for k in range(ncluster)])
+        # re-assign any poorly positioned codebook elements
+        nanix = torch.any(torch.isnan(c), dim=1)
+        ndead = nanix.sum().item()
+        print('done step %d/%d, re-initialized %d dead clusters' % (i+1, niter, ndead))
+        c[nanix] = x[torch.randperm(N)[:ndead]] # re-init dead clusters
+    return c
